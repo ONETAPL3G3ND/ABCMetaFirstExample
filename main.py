35b6a655c97d3459f7c52709ca39dba7ef0f2984
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-05-22 21:46:03 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from abc import ABCMeta, abstractmethod
+
+class Example(metaclass=ABCMeta):
+    @abstractmethod
+    def Example(self):
+        ...
+
+    @abstractmethod
+    def Example2(self):
+        ...
